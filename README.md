c12ec857db39eaf48da96b250fe883a0fc1d892e
thehjer <thehjer9001@gmail.com>
2023-01-22 12:05:31 +0400

    my first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # practice_python
+practicing python and panda-22/01/2023
+
+* matrix labeling matrix_label.py
+* practicing panda from kaggle
---
+++ matrix_label.py
@@ -0,0 +1,15 @@
+#matrix size 8*12
+'''
+test case1:
+python3 matrix_label.py
+3,2
+C02
+test case2:
+python3 matrix_label.py
+8,12
+P12
+test case3:
+python3 matrix_label.py
+3,13
+out of range(matrix range)
+'''
