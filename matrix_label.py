cc3df29fc04775d548dc378e2c324bf5ba07bbb7
thehjer <thehjer9001@gmail.com>
2023-01-26 12:05:20 +0400

    right now

--- matrix_label.py
+++ matrix_label.py
@@ -13,3 +13,30 @@ python3 matrix_label.py
 3,13
 out of range(matrix range)
 '''
+def create_matrix(r,c):
+    matrix=[]
+    for i in range(r):
+        a=[]
+        for j in range(c):
+            k=''
+            a.append(k)
+        matrix.append(a)
+    return(matrix)
+
+def display_matrix(matrix):
+    for i in range(len(matrix[0])):
+        print('*---'*len(matrix[0]),end='')
+        for j in range(len(matrix)):
+            print('|',end='')
+            print((matrix[i][j]),end='')
+        print()
+m=create_matrix(3,3)
+# print(m)
+display_matrix(m)
+
+def label_matrix():
+
+
+
+
+    
